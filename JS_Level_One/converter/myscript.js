7e96f4328dd6f80f86b13589fd53edb6d9c02039
gitdxb <trantambls@gmail.com>
2020-08-16 15:58:13 +0400

    JS converter exercise

---
+++ JS_Level_One/converter/myscript.js
@@ -0,0 +1,15 @@
+// asking for number
+//multiply with *0.454
+//return weight in kg
+//console.log "completed"
+// alert("This is pound - kilogram conversion");
+// var number = prompt("Please enter number of pound you wanna convert below: ");
+// alert("You weight in kilogram is: " + number*0.454);
+// console.log("Conversion completed");
+//
+
+// solution
+var lbs = prompt("Weight in lbs?");
+var kg = lbs*0.454;
+alert("That is: "+kg+ "kilograms");
+console.log("Conversion completed");
