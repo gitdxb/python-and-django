a62dddeb130b0c90a706d142574c0233f50e40aa
gitdxb <trantambls@gmail.com>
2020-08-22 14:12:03 +0400

    sandbox and exercise

---
+++ Python_One/sanbox.py
@@ -0,0 +1,24 @@
+# mystring = 'abcdef'
+# x = mystring.split()
+# print(x)
+
+
+#List
+myList = [1,2,3,'stringabc',True,[3,2,1]]
+listtwo = ['x','y','z']
+#myList[0] = 'New item'
+#myList.append("Another new item")
+# myList.extend(listtwo)
+item = myList.pop()
+
+print(myList)
+print(item)
+
+#Tuples
+
+#boolean
+
+#set
+converted = set([1,1,1,1,2,3,3,2,2,2,2,3,3,3])
+
+print(converted)
